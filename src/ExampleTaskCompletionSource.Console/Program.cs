04215d88b5c5125cf6bea943647a88a89872e6b8
Wodson Correia <wodsonluiz@live.com>
2023-12-12 19:46:42 -0300

    feat: Add new project

---
+++ src/ExampleTaskCompletionSource.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExampleTaskCompletionSource
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(20000);
+                tcs.TrySetResult(42);
+            });
+
+            var result = tcs.Task.GetAwaiter().GetResult();
+            Console.WriteLine($"Resultado da tarefa {result}");
+        }
+    }
+}
